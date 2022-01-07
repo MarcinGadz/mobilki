160c1f4ed3338354c46d32689ca60422b9e37007
message-deleted <229983@edu.p.lodz.pl>
2022-01-07 23:55:45 +0100

    MapComponent works

--- togetherly_front/components/MapComponent.js
+++ togetherly_front/components/MapComponent.js
@@ -1,124 +1,71 @@
 import * as React from "react";
-import { useState } from "react";
-import { StyleSheet, View, Dimensions, PermissionsAndroid } from "react-native";
-import { MapView, Marker } from "react-native-maps";
+import { useState, useEffect } from "react";
+import { StyleSheet, View, Dimensions, Alert } from "react-native";
+import MapView from "react-native-maps";
 import LoadingScreen from "../Screens/LoadingScreen";
-import Geolocation from "react-native-geolocation-service";
+import * as Location from "expo-location";
 
-const MapComponent = ({ regionData }) => {
-  const [region, setRegion] = useState(null);
-  const [permission, setPermission] = useState();
+const MapComponent = () => {
+  const [isLoading, setLoading] = useState(false);
+  const [location, setLocation] = useState(null);
 
-  const getInitialRegion = () => {
-    return {
-      region: {
-        latitude: 51.753745524824076,
-        longitude: 19.45162111534344,
-        latitudeDelta: 0.0,
-        longitudeDelta: 0.0,
-      },
-    };
-  };
-  function getUserLocation() {
-    console.log("o ja $%^&*");
-    Geolocation.getCurrentPosition(
-      (position) => {
-        console.log(position);
-      },
-      (error) => {
-        // See error code charts below.
-        console.log(error.code, error.message);
-      },
-      { enableHighAccuracy: true, timeout: 15000, maximumAge: 10000 }
-    );
-  }
-  const getLocation = async () => {
-    try {
-      var a = await PermissionsAndroid.check(
-        PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION
-      );
-      setPermission(a);
-      console.log("Permission herre " + a);
-      if (permission === (await PermissionsAndroid.RESULTS.GRANTED)) {
-        // has permission
-        setRegion(getUserLocation());
-      } else if (permission === undefined) {
-        // ask for permission
-        setPermission(
-          await PermissionsAndroid.request(
-            PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
-            PermissionsAndroid.PERMISSIONS.ACCESS_COARSE_LOCATION,
-            {
-              title: "Togetherly location permission",
-              message: "Togetherly needs access to your location ",
-              buttonNeutral: "Ask Me Later",
-              buttonNegative: "Cancel",
-              buttonPositive: "OK",
-            }
-          )
-        );
-        console.log("here");
-        setRegion(getUserLocation());
-      } else {
-        // location permission is not granted selecting default region
-        console.log("Location permission denied");
-        setRegion(getInitialRegion());
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        Alert.alert("Permission to access location was denied");
+        setLocation({
+          latitude: 0.0,
+          longitude: 0.0,
+          latitudeDelta: 100,
+          longitudeDelta: 100,
+        });
+        return;
       }
-      console.log(permission);
-    } catch (err) {
-      console.warn(err);
-    }
-  };
 
-  function onRegionChange(region) {
-    setRegion(region);
-  }
-
-  React.useEffect(() => {
-    console.log(region);
-    // if region is provided then set provided region as region
-    if (regionData) {
-      setRegion(regionData);
-    } else {
-      getLocation();
-    }
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation({
+        latitude: location.coords.latitude,
+        longitude: location.coords.longitude,
+        latitudeDelta: 0.01,
+        longitudeDelta: 0.01,
+      });
+      setLoading(false);
+    })();
   }, []);
 
-  if (!region) {
+  if (isLoading) {
     return <LoadingScreen />;
   }
+
   return (
     <View style={styles.container}>
-      <MapView
-        region={region}
-        onRegionChange={onRegionChange()}
-        style={styles.map}
-      ></MapView>
+      <MapView style={styles.map} initialRegion={location} />
     </View>
   );
 
-    // {markers !== undefined &&
-    //   markers.map((marker, index) => (
-    //    <Marker
-    //      key={index}
-    //      coordinate={marker.latlng}
-    //      title={marker.title}
-    //      description={marker.description}
-    //    />
-    //  ))}
+  // {markers !== undefined &&
+  //   markers.map((marker, index) => (
+  //    <Marker
+  //      key={index}
+  //      coordinate={marker.latlng}
+  //      title={marker.title}
+  //      description={marker.description}
+  //    />
+  //  ))}
 };
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: "#fff",
-        alignItems: "center",
-        justifyContent: "center",
-    },
-    map: {
-        width: Dimensions.get("window").width,
-        height: Dimensions.get("window").height,
-    },
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  map: {
+    width: Dimensions.get("window").width,
+    height: Dimensions.get("window").height,
+  },
 });
 
 export default MapComponent;
